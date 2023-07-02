df7dee8bbd674d5d726f3914b8259bfbe4f423ad
bavshehata <bavshehata@gmail.com>
2023-07-03 02:12:06 +0300

    Click events

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const app = Vue.createApp({
   data() {
-    return{
-        title: "The final empire",
-        author: "Brandom Sanerson"
-    }
-  }
+    return {
+      title: "The final empire",
+      author: "Brandom Sanerson",
+      age: 45,
+    };
+  },
+  methods: {
+    changeTitle(title) {
+      this.title = "Words of Randiance";
+      this.title = title;
+    },
+  },
 });
 
 app.mount("#app");
