034d6b29bf7cd69df8593f419c552cc20f73629b
bavshehata <bavshehata@gmail.com>
2023-07-03 03:45:41 +0300

    Attribute binding

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "https://bavlifyweb.com",
       showBooks: true,
       age: 45,
       books: [
-        { title: "name of the wind", author: "patrick rothfuss" },
-        { title: "the way of kings", author: "brandon sanderson" },
-        { title: "the final empire", author: "brandon sanderson" },
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "assets/2.jpg",
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+          img: "assets/3.jpg",
+        },
       ],
       x: 0,
       y: 0,
