f6554d9ddfa5981f0f1ac93c3d2f93184bce77be
bavshehata <bavshehata@gmail.com>
2023-07-14 15:17:47 +0300

    Dynamic links

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,10 +1,12 @@
 <template>
   <h1>Job details</h1>
   <p>Job id is {{ id }}</p>
+  <p>{{}}</p>
 </template>
 
 <script>
 export default {
+  props: ["id"],
   data() {
     return { id: this.$route.params.id };
   },
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>Jobs page</h1>
-  <div v-for="job in jobs" :key="job.id">
-    <h2>{{ job.description }}</h2>
+  <div v-for="job in jobs" :key="job.id" class="job">
+    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }"
+      ><h2>{{ job.description }}</h2></router-link
+    >
   </div>
 </template>
 
@@ -15,9 +17,26 @@ export default {
         { title: "Webdes", id: 1, description: "loremdes" },
         { title: "Webops", id: 2, description: "loremops" },
       ],
+      currentPath: this.$route.params.path,
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+.job h2 {
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
+}
+.job h2:hover {
+  background: #ddd;
+}
+.job a {
+  text-decoration: none;
+}
+</style>
