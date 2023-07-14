e686341315e82e2a924af7c58fa617e0fef79270
bavshehata <bavshehata@gmail.com>
2023-07-14 14:52:25 +0300

    router links

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quae,
+      voluptates, commodi voluptatibus sequi deserunt dolorem animi non, facere
+      at recusandae laudantium earum illo! Nemo debitis cupiditate sequi iusto.
+      Natus, soluta! Aperiam recusandae deleniti dolore consequuntur optio
+      ducimus dicta sapiente non rerum fugit, sed consequatur doloremque, illo
+      assumenda ex, dolor qui? Numquam dolores adipisci quaerat voluptatem
+      perferendis? Doloremque optio voluptatibus veritatis quaerat ratione nihil
+      ipsa soluta molestiae, dolor, eum cupiditate tempore, molestias corrupti
+      ab repellat placeat perspiciatis aut totam quae suscipit numquam. Fugit,
+      fugiat, nihil odit sit velit libero adipisci ad repudiandae reprehenderit
+      aut vitae sapiente aliquid, hic veritatis quibusdam?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quae,
+      voluptates, commodi voluptatibus sequi deserunt dolorem animi non, facere
+      at recusandae laudantium earum illo! Nemo debitis cupiditate sequi iusto.
+      Natus, soluta! Aperiam recusandae deleniti dolore consequuntur optio
+      ducimus dicta sapiente non rerum fugit, sed consequatur doloremque, illo
+      assumenda ex, dolor qui? Numquam dolores adipisci quaerat voluptatem
+      perferendis? Doloremque optio voluptatibus veritatis quaerat ratione nihil
+      ipsa soluta molestiae, dolor, eum cupiditate tempore, molestias corrupti
+      ab repellat placeat perspiciatis aut totam quae suscipit numquam. Fugit,
+      fugiat, nihil odit sit velit libero adipisci ad repudiandae reprehenderit
+      aut vitae sapiente aliquid, hic veritatis quibusdam?
+    </p>
   </div>
 </template>
