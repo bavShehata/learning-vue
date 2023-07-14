6c74a6d48296d1fbaa13eaf1766afef03d07a1fe
bavshehata <bavshehata@gmail.com>
2023-07-14 15:03:21 +0300

    folder structure

---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Jobs page</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <h2>{{ job.description }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jobs",
+  data() {
+    return {
+      jobs: [
+        { title: "Webdev", id: 0, description: "loremdev" },
+        { title: "Webdes", id: 1, description: "loremdes" },
+        { title: "Webops", id: 2, description: "loremops" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
