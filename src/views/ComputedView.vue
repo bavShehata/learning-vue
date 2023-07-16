61def273ea3eb11bd53cca6d7b088a59146f7159
bavshehata <bavshehata@gmail.com>
2023-07-16 14:37:21 +0300

    watch and watcheffect

--- src/views/ComputedView.vue
+++ src/views/ComputedView.vue
@@ -5,15 +5,27 @@
   <div v-for="name in matchingNames" key="name">
     <p>{{ name }}</p>
   </div>
+  <button @click="stopWatching">Stop watching</button>
 </template>
 <script>
-import { computed, ref } from "vue";
+import { computed, ref, watch, watchEffect } from "vue";
 
 export default {
   setup() {
     const names = ref(["Bivo", "Bavly", "Bova"]);
     const search = ref("");
 
+    const stopwatch = watch(search, () => {
+      console.log("Watch function ran");
+    });
+
+    const stopwatchEffect = watchEffect(() => {
+      console.log("Watch effect ran", search.value);
+    });
+    const stopWatching = () => {
+      stopwatch(    );
+      stopwatchEffect(  );
+    };
     const matchingNames = computed(() => {
       return names.value.filter((name) => name.includes(search.value));
     });
@@ -21,6 +33,7 @@ export default {
       names,
       search,
       matchingNames,
+      stopWatching,
     };
   },
 };
