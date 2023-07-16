2a98a94bba5c6b08e9560f2803b9cf925439388a
bavshehata <bavshehata@gmail.com>
2023-07-16 14:31:35 +0300

    Computed properties

---
+++ src/views/ComputedView.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Computed</h1>
+  <input type="text" v-model="search" />
+  <p>Search term: {{ search }}</p>
+  <div v-for="name in matchingNames" key="name">
+    <p>{{ name }}</p>
+  </div>
+</template>
+<script>
+import { computed, ref } from "vue";
+
+export default {
+  setup() {
+    const names = ref(["Bivo", "Bavly", "Bova"]);
+    const search = ref("");
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+    return {
+      names,
+      search,
+      matchingNames,
+    };
+  },
+};
+</script>
+<style></style>
